6532b782325da059968dda09a1430d951280b1f9
TheHiyoko <caio.demoraisfpinto@gmail.com>
2022-02-02 21:31:34 -0300

    estudo 7

---
+++ 2_FUNDAMENTOS/7_console/index.js
@@ -0,0 +1,20 @@
+//mais de um valor
+const a = 10
+const b = 'algum texto'
+const c = [1,2]
+
+console.log(a,b,c)
+
+//contagem de impressão
+console.count('o valor de a é '+a+',contagem:')
+console.count('o valor de a é '+a+',contagem:')
+console.count('o valor de a é '+a+',contagem:')
+console.count('o valor de a é '+a+',contagem:')
+
+//variavel entre string
+console.log('o nome é %s',b)
+
+//limpar o console
+setTimeout(()=> {
+    console.clear()
+},2000)
