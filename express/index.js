55e9bea6bf105d6b3940d6e947e4924c4c1efcd8
TheHiyoko <caio.demoraisfpinto@gmail.com>
2022-03-29 19:26:13 -0300

    some plays

--- express/index.js
+++ express/index.js
@@ -4,17 +4,47 @@ e criar uma rota */
 
 //import express
 const express = require('express')
+const path = require('path')
 
 //iniciando o express
 const app = express()
 const port = 7070//porta 
 
-app.get('/',(req,res) => {
 
-    res.send('hellow world')
+//acessa a pasta do html
+const base_path = path.join(__dirname, 'html')
+
+//* midilleware *
+/*const check_auth = function(req, res, next){
+    
+    req.authStatus = false
+
+    if(req.authStatus){
+        console.log('esta logado')
+        next()
+    }else{
+        console.log('não logado')
+    }
+}app.use(check_auth)*/
+
+
+//rota home
+app.get('/',(req,res) => {
+    //res.send('hellow world')
+
+    res.sendFile(base_path+'/index.html')
 
 })
 
+//parametros
+/*app.get('/users/:id',(req,res) => {
+    const id = req.params.id
+    //resgada o user no banco pelo id
+    console.log('buscando usuário '+id)
+
+    res.sendFile(base_path+'/index copy.html')
+})*/
+
 app.listen(port, () => {
-    console.log('app na porta'+port)
+    console.log('app na porta '+port)
 })
